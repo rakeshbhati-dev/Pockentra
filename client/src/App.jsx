cc739de0beecf70b24d4d6e47ea194e6d8186e4c
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 14:10:43 +0530

    Add route for dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import { Toaster } from "react-hot-toast"
+import Dashboard from "./pages/Dashboard"
 
 function App(){
   return(
@@ -35,6 +36,7 @@ function App(){
       <Routes>
         <Route path='/register' element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="" element={<Dashboard />} />
       </Routes>
     </div>
   )
