e43680b5f28a57f4bb3f7f622ae31d4580c53d42
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 13:52:27 +0530

    Add cors middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const app=express()
 const routes=require('./routes/index.routes')
-
+const cors=require('cors');
+app.use(cors())
 app.use(express.json())
 app.use('/api',routes)
 
