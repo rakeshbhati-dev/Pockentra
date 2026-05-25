de621abc8543c56289135a04820c384e6fd3b014
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 13:50:57 +0530

    Add toast to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,37 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import { Toaster } from "react-hot-toast"
 
 function App(){
   return(
     <div className="bg-[#272c36] min-h-screen" >
+      <Toaster
+  position="top-right"
+  toastOptions={{
+
+    duration: 3000,
+
+    style: {
+      background: "#11141d",
+      color: "#fff",
+    },
+
+    success: {
+      iconTheme: {
+        primary: "#49b8fc",
+        secondary: "#11141d"
+      }
+    },
+
+    error: {
+      iconTheme: {
+        primary: "#ef4444",
+        secondary: "#11141d"
+      }
+    }
+  }}
+/>
       <Routes>
         <Route path='/register' element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
