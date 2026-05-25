8d26ad1f83aaf903a9ace659a4af00eb7f952092
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 14:10:20 +0530

    Create service to get particular user.

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api=import.meta.env.VITE_USER_API
 
-const getUser=async (token)=>{
+export const getUser=async (token)=>{
     const response=await axios.get(`${api}/`,{headers:{Authorization:`Bearer ${token}`}})
     return response.data;
 }
