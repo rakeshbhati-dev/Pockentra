88ff0582d0c4c6eac826737a7a95d7ba7a40b680
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 12:36:53 +0530

    Create Input component

---
+++ client/src/components/Input.jsx
@@ -0,0 +1,28 @@
+function Input({
+    label,
+    name,
+    value,
+    placeholder = '',
+    onChange,
+    type = 'text',
+    inputStyle,
+    divStyle,
+    errorMessage
+}) {
+    return (
+        <div className={`flex-1 ${divStyle || ''}`}>
+            {label && <label className="block text-primary text-xs uppercase tracking-wide mb-1">{label}</label>}
+            <input
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className={`bg-slate-700 rounded-md focus:outline-none focus:ring-1 focus:ring-primary py-2 px-3 w-full text-white placeholder-gray-500 ${inputStyle || ''}`}
+            />
+            {errorMessage && <p className="text-red-500 text-xs pl-1 mt-1">{errorMessage}</p>}
+        </div>
+    )
+}
+
+export default Input
