d22d2973746fff82e5d9e0ef23a8b2614848c998
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-25 12:20:06 +0530

    Add route for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 function App(){
   return(
     <div className="bg-[#272c36] min-h-screen" >
       <Routes>
         <Route path='/register' element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   )
