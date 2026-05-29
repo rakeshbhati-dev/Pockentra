b4852a26688576e2117152574a0dd554721c0579
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-29 13:01:20 +0530

    Add protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,40 +3,48 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import { Toaster } from "react-hot-toast"
 import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./routes/ProtectedRoute"
 
-function App(){
-  return(
+function App() {
+  return (
     <div className="bg-[#272c36] min-h-screen" >
       <Toaster
-  position="top-right"
-  toastOptions={{
+        position="top-right"
+        toastOptions={{
 
-    duration: 3000,
+          duration: 3000,
 
-    style: {
-      background: "#11141d",
-      color: "#fff",
-    },
+          style: {
+            background: "#11141d",
+            color: "#fff",
+          },
 
-    success: {
-      iconTheme: {
-        primary: "#49b8fc",
-        secondary: "#11141d"
-      }
-    },
+          success: {
+            iconTheme: {
+              primary: "#49b8fc",
+              secondary: "#11141d"
+            }
+          },
 
-    error: {
-      iconTheme: {
-        primary: "#ef4444",
-        secondary: "#11141d"
-      }
-    }
-  }}
-/>
+          error: {
+            iconTheme: {
+              primary: "#ef4444",
+              secondary: "#11141d"
+            }
+          }
+        }}
+      />
       <Routes>
         <Route path='/register' element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   )
---
+++ client/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom"
+import { useUser } from "../contexts/UserContextProvider"
+
+function ProtectedRoute({children}){
+    const {token,loading}=useUser()
+    if(loading){
+        return <span className="text-white">Loading..</span>
+    }
+    if(token && !loading){
+        console.log(token)
+        return (children)
+    }
+    else{
+       return(
+        <Navigate to='/login' />
+       )
+    }
+}
+
+export default ProtectedRoute
