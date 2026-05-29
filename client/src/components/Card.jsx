80425793021f1c54ab22774640997a476e181c02
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-29 22:34:15 +0530

    Change card styles

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,14 +2,26 @@ function Card({
     cardStyle,
     title,
     amount,
-    color='text-white'
-}){
-    return(
+    color = 'text-white',
+    iconBg='bg-[#11141d]',
+    icon: Icon
+}) {
+    return (
         <div
-        className={`bg-[#11141d] py-2 rounded-md px-4 ${cardStyle}`}
+            className={`bg-[#11141d] py-2 rounded-md px-4 shadow-sm flex justify-between items-center ${cardStyle}`}
         >
-            <h3 className={`text-xl font-semibold ${color}`}>{title}</h3>
-            <p className={`font-semibold text-2xl ${color}`}>₹{amount}</p>
+            <div>
+                <h3 className={`text-xl font-semibold ${color}`}>{title}</h3>
+                <p className={`font-semibold text-2xl ${color}`}>₹{amount}</p>
+            </div>
+            {Icon &&
+                <div
+                    className={`w-12 h-12 rounded-full flex items-center justify-center ${iconBg}`}
+                >
+                    <Icon className={`${color}`} />
+                </div>
+
+            }
         </div>
     )
 }
