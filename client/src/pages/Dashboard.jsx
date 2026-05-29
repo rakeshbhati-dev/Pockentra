22d2d9990ea83bd510ffd715794d2c785c954908
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-29 22:35:39 +0530

    Add chart and card in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,67 @@
-function Dashboard(){
-    return(
+import { TrendingDown, TrendingUp, Wallet } from "lucide-react";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import Section from "../components/Section";
+import { useUser } from "../contexts/UserContextProvider"
+import { useEffect, useState } from "react";
+import { getExpenseStats, getStats } from "../services/dashboard.service";
+import ExpenseChart from "../components/ExpenseChart";
+
+function Dashboard() {
+    const { user, loading,token } = useUser();
+    const [monthStats,setMonthStats]=useState({
+        totalIncome:0,
+        totalExpense:0,
+        balance:0
+    })
+
+    const [expenseBreakdown,setExpenseBreakdown]=useState([]);
+
+    const getMonthStats=async () => {
+        try {
+           const response= await getStats(token);
+           setMonthStats(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getChartStats=async () => {
+        try {
+            const response=await getExpenseStats(token)
+            console.log(response)
+            setExpenseBreakdown(response.data.breakdown)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        if(token){
+            getMonthStats()
+            getChartStats()
+        }
+    },[user])
+    if (loading) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+    return (
         <>
-        <h1>Dashboard</h1>
+            <Header firstName={user?.firstName} />
+            <Section>
+                <h3 className="text-gray-400 mb-2">This Month</h3>
+                <div className="flex gap-3">
+                    <Card cardStyle='w-1/3 shadow-green-500/20' title='Income' color="text-green-500" amount={monthStats.totalIncome} icon={TrendingUp} iconBg="bg-green-500/20"></Card>
+                    <Card cardStyle='w-1/3 shadow-red-500/20' title='Expense' color="text-red-500" amount={monthStats.totalExpense} icon={TrendingDown} iconBg="bg-red-500/20"></Card>
+                    <Card cardStyle='w-1/3 shadow-primary/20' title='Balance' color="text-primary" amount={monthStats.balance} icon={Wallet} iconBg="bg-primary/20"></Card>
+                </div>
+            </Section>
+
+            <Section>
+                <ExpenseChart breakdown={expenseBreakdown} />
+            </Section>
         </>
     )
 }
