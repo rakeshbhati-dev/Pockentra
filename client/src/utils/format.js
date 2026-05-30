cd3e81a1606a9d04d43a63e703a194b543d7303f
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-30 16:55:48 +0530

    Add helper to format amount

---
+++ client/src/utils/format.js
@@ -0,0 +1,7 @@
+export const formatAmount=(amount)=>{
+   return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 0,
+  }).format(amount);
+}
