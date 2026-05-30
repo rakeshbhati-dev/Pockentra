b77a4ecf21e707f34299f884c3d27c1b23e9fb42
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-05-30 21:36:58 +0530

    Show button is optional

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,18 @@
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.png'
 
-function Header({ firstName }) {
+function Header({ firstName,showButton=true }) {
+    const navigate=useNavigate();
     return (
         <div className='flex px-4 py-3 justify-between items-center bg-[#1f242d]'>
-            <div>
+            <Link to='/'>
                 <img src={logo} alt="" className='w-48' />
-            </div>
+            </Link>
             <div className='flex gap-4'>
-                <button className='bg-primary px-2 rounded-md font-semibold cursor-pointer'>+ Add Transaction</button>
+                {
+                    showButton &&
+                    <button className='bg-primary px-2 rounded-md font-semibold cursor-pointer' onClick={()=>navigate('/transaction/add')}>+ Add Transaction</button>
+                }
                 <div className='w-10 h-10 rounded-full bg-primary flex items-center justify-center font-semibold text-[#fff] cursor-pointer text-xl'>
                 {firstName?.[0].toUpperCase()}
             </div>
