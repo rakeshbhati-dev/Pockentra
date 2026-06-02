351b75afd7837389995def34867d23e10a9ce3b8
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-02 11:44:58 +0530

    Add update profile service

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -6,3 +6,8 @@ export const getUser=async (token)=>{
     const response=await axios.get(`${api}/`,{headers:{Authorization:`Bearer ${token}`}})
     return response.data;
 }
+
+export const updateProfile=async(token,data)=>{
+    const response=await axios.put(`${api}/`,data,{headers:{Authorization:`Bearer ${token}`}})
+    return response.data;
+}
