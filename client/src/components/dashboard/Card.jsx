9ec23ce125493c39d0d984b217e2875804700355
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-05 12:37:36 +0530

    Fix format Amount

--- client/src/components/dashboard/Card.jsx
+++ client/src/components/dashboard/Card.jsx
@@ -1,4 +1,10 @@
-import { formatAmount } from "../../utils/format"
+const formatAmount=(amount)=>{
+   return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 0,
+  }).format(amount);
+}
 
 function Card({
     cardStyle,
